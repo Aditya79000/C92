aa8cfcf57586d3571d2c35b8d6983c0277735c94
Aditya79000 <105648681+Aditya79000@users.noreply.github.com>
2022-10-03 17:53:51 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,9 @@
+function send()
+{
+    number1 = get_word = document.getElementById("number1").value;
+    number2 = get_word = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+}
+document.getElementById("output").innerHTML = row;
+document.getElementById("number1").value ="";
+document.getElementById("number2").value ="";
